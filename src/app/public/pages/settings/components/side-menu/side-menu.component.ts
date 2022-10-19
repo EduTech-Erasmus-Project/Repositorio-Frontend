6c1993db2ced4ce1cfbec4f186d1df9a39a34360
Edw1nM4rquez <edwi1999@hotmail.com>
2022-10-19 10:44:25 -0500

    Internacionalizacion de los componentes

--- src/app/public/pages/settings/components/side-menu/side-menu.component.ts
+++ src/app/public/pages/settings/components/side-menu/side-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { MenuItem } from "primeng/api";
+import { LanguageService } from "src/app/services/language.service";
 import { LoginService } from "../../../../../services/login.service";
 
 @Component({
@@ -10,12 +11,28 @@ import { LoginService } from "../../../../../services/login.service";
 export class SideMenuComponent implements OnInit {
   public items: MenuItem[];
 
-  constructor(private loginService: LoginService) {}
+  constructor(
+    private loginService: LoginService,
+    private languageService:LanguageService
+    ) {}
 
   ngOnInit(): void {
+   this.load_side_menu();
+   
+  }
+  onQuit() {
+    //console.log("quit")
+    this.loginService.signOut();
+  }
+
+  private async load_side_menu(){
+    let exit = await this.languageService.translate.get("menu.sideMenu.exit").toPromise();
+    let security = await this.languageService.translate.get("menu.sideMenu.security").toPromise();
+    let myAccount = await this.languageService.translate.get("menu.sideMenu.myAccount").toPromise();
+    
     this.items = [
       {
-        label: "Mi perfil",
+        label: myAccount,
         icon: "pi pi-fw pi-user-edit",
         routerLink: "profile",
         routerLinkActiveOptions: {
@@ -24,7 +41,7 @@ export class SideMenuComponent implements OnInit {
         },
       },
       {
-        label: "Seguridad",
+        label: security,
         icon: "pi pi-fw pi-key",
         routerLink: "security",
         routerLinkActiveOptions: {
@@ -36,73 +53,70 @@ export class SideMenuComponent implements OnInit {
         separator: true,
       },
       {
-        label: "Salir",
+        label: exit,
         icon: "pi pi-fw pi-power-off",
         command: (event) => {
           this.onQuit();
         },
       },
-    ];
-
-    //console.log("role student", this.loginService.user)
-
-    if (this.loginService.validateRole("student")) {
-      this.items.splice(2, 0, {
-        label: "Vistos por mi",
-        icon: "pi pi-fw pi-list",
-        routerLink: "my-views",
-        routerLinkActiveOptions: {
-          exact: true,
-          styleClass: "router-active",
-        },
-      });
-      this.items.splice(1, 0, {
-        label: "Calificados por mi",
-        icon: "pi pi-fw pi-check",
-        routerLink: "objects-qualified",
-        routerLinkActiveOptions: {
-          exact: true,
-          styleClass: "router-active",
-        },
-      });
-    }
-
-    if (this.loginService.validateRole("teacher")) {
-      this.items.splice(2, 0, {
-        label: "Mis Objetos de aprendizaje",
-        icon: "pi pi-fw pi-list",
-        routerLink: "my-objects",
-        routerLinkActiveOptions: {
-          exact: true,
-          styleClass: "router-active",
-        },
-      });
-
-      this.items.splice(3, 0, {
-        label: "Subir Objeto de aprendizaje",
-        icon: "pi pi-fw pi-upload",
-        routerLink: "new-object",
-        routerLinkActiveOptions: {
-          exact: true,
-          styleClass: "router-active",
-        },
-      });
-    }
-    
-    if (this.loginService.validateRole("expert")) {
+    ]; 
+     //console.log("role student", this.loginService.user)
+    let viewI = await this.languageService.translate.get("recommended.viewed").toPromise();
+    let qualificateI = await this.languageService.translate.get("menu.sideMenu.ratedMe").toPromise();
+      if (this.loginService.validateRole("student")) {
+        this.items.splice(2, 0, {
+          label: viewI,
+          icon: "pi pi-fw pi-list",
+          routerLink: "my-views",
+          routerLinkActiveOptions: {
+            exact: true,
+            styleClass: "router-active",
+          },
+        });
         this.items.splice(1, 0, {
-          label: "Objetos de aprendizaje calificados",
-        icon: "pi pi-fw pi-check",
-        routerLink: "objects-qualified",
-        routerLinkActiveOptions: {
-          exact: true,
-          styleClass: "router-active",
-        },
-        })
-    }
-  }
-  onQuit() {
-    //console.log("quit")
-    this.loginService.signOut();
+          label: qualificateI,
+          icon: "pi pi-fw pi-check",
+          routerLink: "objects-qualified",
+          routerLinkActiveOptions: {
+            exact: true,
+            styleClass: "router-active",
+          },
+        });
+      }
+  
+      if (this.loginService.validateRole("teacher")) {
+        this.items.splice(2, 0, {
+          label: "Mis Objetos de aprendizaje",
+          icon: "pi pi-fw pi-list",
+          routerLink: "my-objects",
+          routerLinkActiveOptions: {
+            exact: true,
+            styleClass: "router-active",
+          },
+        });
+  
+        this.items.splice(3, 0, {
+          label: "Subir Objeto de aprendizaje",
+          icon: "pi pi-fw pi-upload",
+          routerLink: "new-object",
+          routerLinkActiveOptions: {
+            exact: true,
+            styleClass: "router-active",
+          },
+        });
+      }
+      
+      if (this.loginService.validateRole("expert")) {
+          this.items.splice(1, 0, {
+            label: "Objetos de aprendizaje calificados",
+          icon: "pi pi-fw pi-check",
+          routerLink: "objects-qualified",
+          routerLinkActiveOptions: {
+            exact: true,
+            styleClass: "router-active",
+          },
+          })
+      }
+
   }
 }
